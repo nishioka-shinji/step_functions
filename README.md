7569c1c519be4a969bb2fd5097a0c4e276e58556
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2022-01-23 07:47:21 +0900

    テーブルアイテム追加

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # 目的
 こちらのハンズオンの学習
+
 [AWS Hands-on for Beginners AWS Step Functions 入門 - ビジュアルツールを使ってローコードにワークフローを作成する](https://pages.awscloud.com/JAPAN-event-OE-Hands-on-for-Beginners-StepFunctions-2022-reg-event.html)
 
 DynamoDBをTerraformで用意
--- dynamodb.tf
+++ dynamodb.tf
@@ -60,3 +60,27 @@ resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
     target_value = 70
   }
 }
+
+# テーブルアイテム
+resource "aws_dynamodb_table_item" "items" {
+  for_each = {
+    item1 = {
+      index = "0001"
+      something = "説明0001"
+    }
+    item2 = {
+      index = "0002"
+      something = "説明0002"
+    }
+  }
+  
+  table_name = aws_dynamodb_table.basic-dynamodb-table.name
+  hash_key   = aws_dynamodb_table.basic-dynamodb-table.hash_key
+
+  item = <<EOF
+{
+  "${aws_dynamodb_table.basic-dynamodb-table.hash_key}": {"S": "${each.value.index}"},
+  "detail": {"S": "${each.value.something}"}
+}
+EOF
+}
